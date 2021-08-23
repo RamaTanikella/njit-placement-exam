3eb7d88c5e1aa93667df9a79b7a3979a12b267db
Rama <rama.t99@gmail.com>
2021-08-23 18:33:25 -0400

    Final Exam Submission

--- assets/script.js
+++ assets/script.js
@@ -2,7 +2,7 @@
 console.log('JavaScript code has loaded!')
 
 // First, tell us your name
-let yourName = "Jane Doe" // HINT: Replace this with your own name!
+let yourName = "Rama Tanikella" // HINT: Replace this with your own name!
 
 // We'll use these variables to track the counts of each cookie type
 let gb = 0      // Ginger bread
@@ -14,10 +14,45 @@ document.getElementById('credit').textContent = `Created by ${yourName}`
 
 // Event listener for clicks on the "+" button for Ginger Bread cookies
 document.getElementById('add-gb').addEventListener('click', function() {
-    // HINT: You can delete this console.log after you no longer need it!
-    console.log('Ginger bread + button was clicked!')
+    gb+=1
+    document.getElementById("qty-gb").innerText = gb
+    document.getElementById("qty-total").innerText = gb+cc+sugar
 
-    // TODO: Write the code to be run when the "+" button for "Ginger Bread" is clicked
 })
 
-// TODO: Hook up event listeners for the rest of the buttons
+document.getElementById('minus-gb').addEventListener('click', function() {
+    if(gb>0){
+        gb-=1
+        document.getElementById("qty-gb").innerText = gb
+        document.getElementById("qty-total").innerText = gb+cc+sugar
+    }
+})
+
+document.getElementById('add-cc').addEventListener('click', function() {
+    cc+=1
+    document.getElementById("qty-cc").innerText = cc
+    document.getElementById("qty-total").innerText = gb+cc+sugar
+})
+
+document.getElementById('minus-cc').addEventListener('click', function() {
+    if(cc>0){
+        cc-=1
+        document.getElementById("qty-cc").innerText = cc
+        document.getElementById("qty-total").innerText = gb+cc+sugar
+    }
+})
+
+document.getElementById('add-sugar').addEventListener('click', function() {
+    sugar+=1
+    document.getElementById("qty-sugar").innerText = sugar
+    document.getElementById("qty-total").innerText = gb+cc+sugar
+})
+
+document.getElementById('minus-sugar').addEventListener('click', function() {
+    if(sugar>0){
+        sugar-=1
+        document.getElementById("qty-sugar").innerText = sugar
+        document.getElementById("qty-total").innerText = gb+cc+sugar
+    }
+})
+
